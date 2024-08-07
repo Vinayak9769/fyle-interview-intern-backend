a6dee8aec216ac4bcd1dfbe009bf299673789022
Vinayak9769 <vinayak97696@gmail.com>
2024-08-07 21:54:51 +0530

    More test fixes

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,21 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+WITH TeacherGradingCount AS (
+    SELECT
+        teacher_id,
+        COUNT(*) AS total_graded
+    FROM
+        assignments
+    GROUP BY
+        teacher_id
+    ORDER BY
+        total_graded DESC
+    LIMIT 1
+)
+SELECT
+    COUNT(*) AS grade_a_count
+FROM
+    assignments
+WHERE
+    grade = 'A'
+    AND teacher_id = (SELECT teacher_id FROM TeacherGradingCount);
+
--- tests/principals_test.py
+++ tests/principals_test.py
@@ -34,7 +34,7 @@ def test_grade_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 4,
+            'id': 1,  #CHANGED ID FROM DRAFTED TO A GRADED ASSIGNMENT
             'grade': GradeEnum.C.value
         },
         headers=h_principal
@@ -49,7 +49,7 @@ def test_regrade_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 4,
+            'id': 2, #CHANGED ID FROM DRAFTED TO A GRADED ASSIGNMENT
             'grade': GradeEnum.B.value
         },
         headers=h_principal
