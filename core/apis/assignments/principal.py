70d4ebfb339d75d84932532fbd5539aa18484883
Vinayak9769 <vinayak97696@gmail.com>
2024-08-07 12:56:38 +0530

    Added /principal/assignments/grade

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
+from core.models.assignments import Assignment, AssignmentStateEnum
 from core.models.teachers import Teacher
-from .schema import AssignmentSchema, AssignmentSubmitSchema, TeacherSchema
+from .schema import AssignmentSchema, AssignmentSubmitSchema, TeacherSchema, AssignmentGradeSchema
 
 principal_assignments_resources = Blueprint('principal', __name__)
 
@@ -12,19 +12,39 @@ principal_assignments_resources = Blueprint('principal', __name__)
 @principal_assignments_resources.route('/assignments', methods=['GET'])
 @decorators.authenticate_principal
 def list_assignments(p):
+    """Returns list of assignments"""
     assignments = Assignment.get_submitted_and_graded()
     assignment_schema = AssignmentSchema(many=True)
     result = assignment_schema.dump(assignments)
-    return jsonify({'data': result})
+    return APIResponse.respond(data=result)
 
 
 @principal_assignments_resources.route('/teachers', methods=['GET'])
 @decorators.authenticate_principal
 def list_teachers(p):
+    """Returns list of teachers"""
     teachers = Teacher.get_all_teachers()
     teacher_schema = TeacherSchema(many=True)
     result = teacher_schema.dump(teachers)
+    return APIResponse.respond(data=result)
 
-    # Return the result as JSON response
-    return jsonify({'data': result})
 
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade or Re-grade an assignment"""
+    assignment_data = AssignmentGradeSchema().load(incoming_payload)
+    assignment_id = getattr(assignment_data, 'id', None)
+    grade = getattr(assignment_data, 'grade', None)
+    if assignment_id is None or grade is None:
+        return APIResponse.respond(status=400, message='Invalid data provided')
+    assignment = Assignment.get_by_id(assignment_id)
+    if not assignment:
+        return APIResponse.respond(status=404, message='Assignment not found')
+    assignment.grade = grade
+    assignment.state = AssignmentStateEnum.GRADED
+    db.session.commit()
+    assignment_schema = AssignmentSchema()
+    result = assignment_schema.dump(assignment)
+    return APIResponse.respond(data=result)
