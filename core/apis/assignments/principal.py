b41f9dd23e63d01e80db1480709601ff570ba746
Vinayak9769 <vinayak97696@gmail.com>
2024-08-07 12:33:32 +0530

    Added /principal/teachers

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -3,9 +3,11 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
-from .schema import AssignmentSchema, AssignmentSubmitSchema
+from core.models.teachers import Teacher
+from .schema import AssignmentSchema, AssignmentSubmitSchema, TeacherSchema
 
 principal_assignments_resources = Blueprint('principal', __name__)
+
 
 @principal_assignments_resources.route('/assignments', methods=['GET'])
 @decorators.authenticate_principal
@@ -13,5 +15,16 @@ def list_assignments(p):
     assignments = Assignment.get_submitted_and_graded()
     assignment_schema = AssignmentSchema(many=True)
     result = assignment_schema.dump(assignments)
+    return jsonify({'data': result})
+
+
+@principal_assignments_resources.route('/teachers', methods=['GET'])
+@decorators.authenticate_principal
+def list_teachers(p):
+    teachers = Teacher.get_all_teachers()
+    teacher_schema = TeacherSchema(many=True)
+    result = teacher_schema.dump(teachers)
+
+    # Return the result as JSON response
     return jsonify({'data': result})
 
